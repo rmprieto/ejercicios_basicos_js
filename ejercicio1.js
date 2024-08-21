ee15269310936e4348ee3ae8f9b18bac7cf3f210
rmprieto <COLD_BITTER@movistar.es>
2024-08-21 21:22:54 +0200

    Create ejercicio1.js

---
+++ ejercicio1.js
@@ -0,0 +1,13 @@
+//1.1 - Define tu superheroe favorito
+let miSuperheroeFavorito = "Hulk";
+//1.2 - Define un numero
+let numeroFavorito = 50;
+//1.3 - Define las dimensiones
+let altura = 5;
+let longitud = 10;
+//1.4 - Calcula la suma
+let suma = altura + longitud;
+//1.5 - Imprime los resultados
+console.log("Mi superheroe favorito es " + miSuperheroeFavorito);
+console.log("Mi numero favorito es el " + numeroFavorito);
+console.log("La suma de la altura(" + altura + ") y la longitud(" + longitud + ") es " + suma);
