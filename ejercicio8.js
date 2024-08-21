c4ca5cf2d4261a024dda8087c329d7a103d153d0
rmprieto <COLD_BITTER@movistar.es>
2024-08-21 21:23:25 +0200

    Create ejercicio8.js

---
+++ ejercicio8.js
@@ -0,0 +1,15 @@
+const avengers = ['Hulk', 'Thor', 'Iron Man', 'Captain A.', 'Spiderman', 'Captain M.'];
+
+function findLongestWord(stringList) {
+  let longestWord = ""; 
+
+  for (let i = 0; i < stringList.length; i++) {
+    if (stringList[i].length > longestWord.length) {
+      longestWord = stringList[i]; 
+    }
+  }
+
+  return longestWord; 
+}
+
+console.log(findLongestWord(avengers));
