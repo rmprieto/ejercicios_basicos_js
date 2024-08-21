abbda3772b946bff26ce4d3373a95f1779cfd081
rmprieto <COLD_BITTER@movistar.es>
2024-08-21 21:23:49 +0200

    Create ejercicio16.js

---
+++ ejercicio16.js
@@ -0,0 +1,12 @@
+const placesToTravel = [
+    'Japon', 
+    'Venecia', 
+    'Murcia', 
+    'Santander', 
+    'Filipinas', 
+    'Madagascar'
+];
+
+for (const placeToTravel of placesToTravel){
+    console.log(placeToTravel);
+}
